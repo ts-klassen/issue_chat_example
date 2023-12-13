925887d6f059689aa88d11ff093706d6dfcdf59f
Ubuntu <git@su-shiki.com>
2023-12-13 23:33:48 +0000

    prototype

--- apps/issue_chat_example/src/issue_chat_example_sup.erl
+++ apps/issue_chat_example/src/issue_chat_example_sup.erl
@@ -26,10 +26,29 @@ start_link() ->
 %%                  type => worker(),       % optional
 %%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 3,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [
+        #{
+            id => issue_chat_example_on_issue_open_webhook_worker
+          , start => {issue_chat_example, on_issue_open_webhook_worker, []}
+          , restart => permanent
+          , type => worker
+        }
+      , #{
+            id => issue_chat_example_on_comment_create_webhook_worker
+          , start => {issue_chat_example, on_comment_create_webhook_worker, []}
+          , restart => permanent
+          , type => worker
+        }
+      , #{
+            id => issue_chat_example_run_worker
+          , start => {issue_chat_example, run_worker, []}
+          , restart => permanent
+          , type => worker
+        }
+    ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
